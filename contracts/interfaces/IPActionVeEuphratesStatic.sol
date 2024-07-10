c8c2ae31ecc2ece2992e966d6877e79d351fa9ff
Fractle Finance <160112002+Fractle-Finance@users.noreply.github.com>
2024-07-10 09:12:08 +0800

    Delete contracts/interfaces/IPActionVeEuphratesStatic.sol

--- contracts/interfaces/IPActionVeEuphratesStatic.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.0;
-
-interface IPActionVeFractleStatic {
-    function increaseLockPositionStatic(
-        address user,
-        uint128 additionalAmountToLock,
-        uint128 newExpiry
-    ) external view returns (uint128 newVeBalance);
-}
